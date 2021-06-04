001954dd1c0f0d35d5083d340c66e81be7f3b636
leoli51 <leoli51@users.noreply.github.com>
2021-06-04 18:06:04 +0200

    Started work on power up system

---
+++ Assets/scripts/PowerUpSystem/PowerUp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float decision_time;
+    float elapsed_time;
+
+    bool player_1_in;
+    bool player_2_in;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_1_in = false;
+        player_2_in = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player_1_in && player_2_in)
+            elapsed_time += Time.deltaTime;
+        else
+            elapsed_time = 0;
+
+        if (elapsed_time > decision_time)
+        {
+            Consume();
+            transform.parent.parent.GetComponent<PowerUpManager>().Clear();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player1")
+            player_1_in = true;
+        if (other.tag == "Player2")
+            player_2_in = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player1")
+            player_1_in = false;
+        if (other.tag == "Player2")
+            player_2_in = false;
+    }
+
+    void Consume() 
+    {
+        Debug.Log("Consumed power up");
+    }
+}
---
+++ Assets/scripts/PowerUpSystem/PowerUpManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour
+{
+
+    public List<GameObject> power_ups;
+
+    Transform power_up1_transform;
+    Transform power_up2_transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        power_up1_transform = transform.GetChild(0);
+        power_up2_transform = transform.GetChild(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void Show()
+    {
+        // chooses 2 power ups 
+        // instantiate them
+        GameObject p1 = Instantiate(power_ups[Random.Range(0, power_ups.Count)]) as GameObject;
+        GameObject p2 = Instantiate(power_ups[Random.Range(0, power_ups.Count)]) as GameObject;
+
+        p1.transform.parent = power_up1_transform;
+        p2.transform.parent = power_up2_transform;
+    }
+
+    public void Clear()
+    {
+        // destroy children 
+        Destroy(power_up1_transform.GetChild(0));
+        Destroy(power_up2_transform.GetChild(0));
+    }
+
+}
