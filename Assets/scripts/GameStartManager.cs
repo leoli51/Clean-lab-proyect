b350a7f813365ca97507a38bfc023baa3895aa74
leoli51 <leoli51@users.noreply.github.com>
2021-06-06 17:18:33 +0200

    GameStartManager

---
+++ Assets/scripts/GameStartManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartManager : MonoBehaviour
+{
+
+    public float time_to_disappear = .75f;
+
+    PlayerStartPosition p1, p2;
+    bool game_started = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1 = transform.GetChild(0).GetComponent<PlayerStartPosition>();
+        p2 = transform.GetChild(1).GetComponent<PlayerStartPosition>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (p1.player_ready && p2.player_ready && !game_started)
+        {
+            game_started = true;
+            p1.disappear(time_to_disappear);
+            p2.disappear(time_to_disappear);
+
+            StartGame();
+        }
+        if (game_started)
+        {
+            time_to_disappear -= Time.deltaTime;
+            if (time_to_disappear <= 0)
+                Destroy(this.gameObject);
+        }
+    }
+
+    void StartGame()
+    {
+        Debug.Log("Game is starting what should we do?");
+    }
+}
---
+++ Assets/scripts/PlayerStartPosition.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerStartPosition : MonoBehaviour
+{
+
+    public string player_tag;
+
+    [HideInInspector]
+    public bool player_ready = false;
+
+    bool disappearing = false;
+    float disappear_time;
+    float elapsed_disappear_time = 0;
+
+    SpriteRenderer sprite;
+    TextMeshPro text;
+
+    Color sprite_start_color;
+    Color text_start_color;
+
+    Color transparent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        text = GetComponentInChildren<TextMeshPro>();
+
+        sprite_start_color = sprite.color;
+        text_start_color = text.color;
+        transparent = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (disappearing)
+        {
+            elapsed_disappear_time += Time.deltaTime;
+            float disappear_lerp_value = Mathf.Clamp01(elapsed_disappear_time / disappear_time);
+            sprite.color = Color.Lerp(sprite_start_color, transparent, disappear_lerp_value);
+            text.color = Color.Lerp(text_start_color, transparent, disappear_lerp_value);
+        }
+            
+    }
+
+    public void disappear(float disappear_time)
+    {
+        this.disappear_time = disappear_time;
+        disappearing = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == player_tag)
+            player_ready = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == player_tag)
+            player_ready = true;
+    }
+}
